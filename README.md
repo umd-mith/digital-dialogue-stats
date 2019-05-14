9a7fb8e2b14e618b24f67b4955f318d1efe7fc40
Ed Summers <ehs@pobox.com>
2019-05-14 15:37:20 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+You will need an access token for the Vimeo API, and set it in your environment
+as ACCESS_TOKEN. 
---
+++ stats.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import csv
+import requests
+from os import environ as env
+
+token = env.get('ACCESS_TOKEN')
+
+def get(path, params):
+    url = 'https://api.vimeo.com/' + path
+    resp = requests.get(
+        url,
+        params=params,
+        headers={'Authorization': 'bearer ' + token})
+    return resp.json()
+
+page = 0
+out = csv.writer(open('stats.csv', 'w'))
+out.writerow(['title', 'created', 'plays'])
+
+while True:
+    page += 1
+    results = get('me/videos', {"page": page})
+
+    if 'data' not in results:
+        break
+
+    for video in results['data']:
+        out.writerow([
+            video['name'],
+            video['created_time'],
+            video['stats']['plays'],
+            'https://vimeo.com' + video['uri']
+        ])
+
+
+
+
+
+
+
+
+
+
+
+
